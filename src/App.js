e379b94cd36ddcbfc58c87f2969c4551a7b03ac0
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-03-20 10:13:09 +0500

    Start Header set

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import SingleProducts from "./components/SingleProducts/SingleProducts";
 import Header from "./components/Header/Header"
 import Newsletter from "./components/Footer/Newslatter/Newslatter"
 import Footer from "./components/Footer/Footer"
+import AppContext from "./utils/context";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header/>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/category/:id" element={<Category />}/>
-          <Route path="/singleproducts/:id" element={<SingleProducts />}/>
-        </Routes>
-        <Newsletter /> 
-        <Footer />
+        <AppContext>
+          
+          <Header/>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/category/:id" element={<Category />}/>
+            <Route path="/singleproducts/:id" element={<SingleProducts />}/>
+          </Routes>
+          <Newsletter /> 
+          <Footer />
+          
+        </AppContext>
       </BrowserRouter>
     </div>
   );
