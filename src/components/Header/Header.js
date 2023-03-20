059f697cc6af0d84671f282174a4764f696f4205
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-03-20 10:42:32 +0500

    Create Navbar and icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import Search from './Search/Search'
+import Cart from '../Cart/Cart'
+import { Context } from '../../utils/context'
+import {BsSearch} from 'react-icons/bs'
+import {HiOutlineShoppingCart} from 'react-icons/hi'
 
 function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <header className='main-header'>
+      <div className="header-content">
+        <ul className="letf">
+          <li>Home</li>
+          <li>About</li>
+          <li>Categorys</li>
+        </ul>
+      </div>
+      <div className="center">Hello</div>
+      <div className="right">
+        <BsSearch/>
+        <span className="card-icon">
+          <HiOutlineShoppingCart/>
+        </span>
+      </div>
+    </header>
   )
 }
 
