fa25b8e0a4cb538c4d7a7de7b59e9d4ad81ef751
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-03-20 09:52:59 +0500

    Add Files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import {BrowserRouter,Route, Routes} from "react-router-dom";
+import Home from "./components/Home/Home";
+import Category from "./components/Category/Category";
+import SingleProducts from "./components/SingleProducts/SingleProducts";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/category/:id" element={<Category />}/>
+          <Route path="/singleproducts/:id" element={<SingleProducts />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/SingleProducts/SingleProducts.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SingleProducts() {
+  return (
+    <div>
+      SingleProducts
+    </div>
+  )
+}
+
+export default SingleProducts
